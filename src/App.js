5e1284c6d81da529d06243634d06e2bc4bf62310
ivankvitka <ivan.kvitka@gmail.com>
2019-04-30 17:15:06 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Styles/main.scss';
+import Header from './Components/Header';
+import Routing from './Components/Routing';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Routing/>        
+      </div>
+    );
+  }
+}
+
+export default App;
